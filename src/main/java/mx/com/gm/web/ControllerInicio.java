84d18ce69ed21b27cfabe3217db54c0ba6610f57
becario.fernando <fer@hotmail.com>
2022-06-16 17:39:00 -0500

    operacion dao basica

--- src/main/java/mx/com/gm/web/ControllerInicio.java
+++ src/main/java/mx/com/gm/web/ControllerInicio.java
@@ -2,7 +2,9 @@ package mx.com.gm.web;
 
 import lombok.extern.slf4j.Slf4j;
 
+import mx.com.gm.dao.PersonaDao;
 import mx.com.gm.domain.Persona;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,8 +16,13 @@ import java.util.List;
 @Controller
 @Slf4j
 public class ControllerInicio {
+    @Autowired
+    private PersonaDao personaDao;
+
   @GetMapping("/")
   public String incio(Model model) {
+      var personas = personaDao.findAll();
+      //Iterable<Persona> personas1 = personaDao.findAll();
 //    String mensaje = "Hola mundo con thymeleaf";
 //    Persona persona = new Persona();
 //    persona.setNombre("Juan");
@@ -33,7 +40,7 @@ public class ControllerInicio {
 //
 //    model.addAttribute("mensaje", mensaje);
 //    //model.addAttribute("persona", persona);
-//    model.addAttribute("personas", personas);
+      model.addAttribute("personas", personas);
 //    // log.info("Ejecuntado el controlador spring MVC");
  return "index";
   }
